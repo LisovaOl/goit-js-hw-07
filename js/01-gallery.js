a4f4820c8d6ec1561d268a6c50db9817ddb52214
LisovaOl <lisova.olenka@gmail.com>
2022-07-29 07:46:25 +0300

    02 first

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -13,7 +13,7 @@ function createImgMarkup(galleryItems) {
     .map(({ preview, original, description }) => {
       return `
     <div class="gallery__item">
-  <a class="gallery__link" data-lightbox = "model" href="${original}">
+  <a class="gallery__link" href="${original}">
     <img
       class="gallery__image"
       src="${preview}"
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,28 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+const imgMarkup = createImgMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", imgMarkup);
+
+function createImgMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+  <a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>`;
+    })
+    .join("");
+}
+
+{/* <a class="gallery__item" href="large-image.jpg">
+  <img class="gallery__image" src="small-image.jpg" alt="Image description" />
+</a>; */}
